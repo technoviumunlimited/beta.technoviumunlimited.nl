30c438fc478168fae916cc3aa6fe0094e0db5697
davor <dradic@roc-nijmegen.nl>
2023-10-13 16:52:02 +0200

    new push with blogs

---
+++ _nuxt/logout.22c8e3b8.js
@@ -0,0 +1,1 @@
+import{h as t,D as e,C as r,j as s,p as a}from"./entry.1f11a814.js";const l={__name:"logout",setup(n){return t(async()=>{try{const o=await e();console.log("Logout user:",o),r("/login")}catch(o){console.error(o),state.errors.push(o)}}),(o,c)=>(a(),s("div",null," Logging out... "))}};export{l as default};
